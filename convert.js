ed163d19dcb358d036cf1999b095567aba212588
T_Higgins <70584458+Th876@users.noreply.github.com>
2021-10-28 04:36:48 -0400

    Simplified JS

--- convert.js
+++ convert.js
@@ -1,22 +1,12 @@
 const fahrenheitInput = document.getElementById("fahrenheit"); //create fahrenheit variable & link to HTML ID
 const celsiusInput = document.getElementById("celsius");  //create celsius variable & link to HTML ID
-const inputs = document.getElementsByClassName("inputs__input");  //create inputs variable & link to HTML class name
 
-for (let i = 0; i < inputs.length; i++) {
-    let input = inputs[i];
+celsiusInput.oninput = () => { //let input for celsius convert to fahrenheit immediately with oninput attribute 
+    let output = (parseFloat(celsiusInput.value) * 9) /5 +32; //convert celsius input to fahrenheit and reserve decimals
+    fahrenheitInput.value = parseFloat(output.toFixed(2)); //convert value entered to two decimal places
+};
 
-    input.addEventListener("input", function (e) { //add listener for input
-        let value = parseFloat(e.target.value); //convert value into a float
-       
-        switch (e.target.name) { //create switch statements for the numbers entered under both degree headings
-            case "fahrenheit":
-                celsiusInput.value = (value-32) / 1.8;
-                break;
-            case "celsius":
-                fahrenheitInput.value = (value * 1.8) + 32; 
-                break;
-            default:
-                break;
-        }
-    });
-}
+fahrenheitInput.oninput = () => { //let input for fahrenheit convert to celsius immediately with oninput attribute 
+    let output = (parseFloat(fahrenheitInput.value) - 32) * 5 /9; //convert fahrenheit input to celsius and reserve decimals
+    celsiusInput.value = parseFloat(output.toFixed(2)); //convert value entered to two decimal places
+};
